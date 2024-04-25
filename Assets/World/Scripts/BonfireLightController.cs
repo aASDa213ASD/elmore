dc221d194bc301a84c6a671a91ac615e01878d93
aASDa213ASD <ragebladesamurai@gmail.com>
2024-04-25 11:44:53 +0300

    Misc

--- Assets/World/Scripts/BonfireLightController.cs
+++ Assets/World/Scripts/BonfireLightController.cs
@@ -6,24 +6,27 @@ using UnityEngine.Rendering.Universal;
 public class BonfireLightController : MonoBehaviour
 {
     private Light2D bonfire_light;
-    private float noise;
+
+    private float speed = 2.0f;
+    private float noise = 0.0f;
+    private float seed  = 0.0f;
+
     private float min_falloff = 0.5f;
     private float max_falloff = 0.8f;
 
     private float min_intensity = 3.0f;
     private float max_intensity = 5.0f;
 
-    private float speed = 1.5f;
-
+    // TODO: Add random in range ^
     void Start()
     {
         bonfire_light = GetComponent<Light2D>();
+        seed = Random.Range(0f, 100f);
     }
 
     void Update()
     {
-        noise = Mathf.PerlinNoise(Time.time * speed, 0.0f);
-
+        noise = Mathf.PerlinNoise((Time.time + seed) * speed, 0.0f);
         bonfire_light.falloffIntensity = Mathf.Lerp(min_falloff, max_falloff, noise);
         bonfire_light.intensity = Mathf.Lerp(min_intensity, max_intensity, noise);
     }
